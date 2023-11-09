cbfe036a3289081d22f62becf68ddbcd3a5852fc
alineferroarq <alineferroarq@gmail.com>
2023-11-09 20:09:17 -0300

    back end subindo o servidor

---
+++ readme.md
@@ -0,0 +1,5 @@
+abrir pasta no vs code 
+criar o arquivo server.js
+no terminal
+$ npm init -y
+$ npm i cors dotenv express mysql2 nodemon sequelize 
---
+++ server.js
@@ -0,0 +1,18 @@
+import  express  from 'express'
+
+const app = express()
+
+app.use(express.json()) //express vai configurar metodos http na aplicação
+
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.json({message:'servidor rodando'})
+})
+
+const HOST = 'localhost'
+const PORT = '5000'
+
+app.listen(PORT, () => {
+ console.log(`servidro rodando em : http://${HOST}:${PORT}`)
+})
