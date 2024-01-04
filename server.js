6de1db0253124600d0b84989982ceccee98cb7b8
ALINE FERRO BEZERRA <ALINE6438@DIGITALCOLLEGE.LOCAL>
2024-01-04 18:47:29 -0300

    cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import  express  from 'express'
 import db from './model/index.js'
 import { routerProduto } from './routes/produto.routes.js'
+import cors from "cors"
+
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json()) //express vai configurar metodos http na aplicação
 
